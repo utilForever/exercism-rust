ecd08e794d0d736eec206d7e35822c54692dd1bc
Chris Ohk <utilforever@gmail.com>
2023-04-08 15:27:52 +0900

    Upload exercise "Macros"

---
+++ macros.rs
@@ -0,0 +1,17 @@
+#[macro_export]
+macro_rules! hashmap {
+    () => {
+        ::std::collections::HashMap::new()
+    };
+    ( $( $key:expr => $value:expr ),* $(,)? ) => {
+        {
+            let mut map = ::std::collections::HashMap::new();
+
+            $(
+                map.insert($key, $value);
+            )+
+
+            map
+        }
+    };
+}
